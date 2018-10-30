01a4b037aeb979e3fa2e51b1ef7f17579a34fc68
Maksim <mbkkong@gmail.com>
2018-10-30 14:34:26 +0200

    Доделал ДЗ 6. Часть 1

--- homework6/work_1.js
+++ homework6/work_1.js
@@ -1,5 +1,8 @@
-let array = ['first', 'second'];
+/*
+    Надеюсь я понял задачу Ирина)
+ */
 
+let array = ['one', 'second', 'three', 'cetire', 'ElemenBlet'];
 
 //  Создаём стили для страницы сайта
 //   Чтобы всё по красоте было
@@ -8,19 +11,12 @@ let stylesText = document.createTextNode(`
     html{ background: url('https://steamusercontent-a.akamaihd.net/ugc/857225545729970715/C3381211D6F4825D63C4AF75E9827C5DA79D50DC/')}
     input{ padding: 10px 20px; border: 0; display: inline-block}
     label{ color: #ffffff; background-color: red}
-    section {background: cyan;}
+    section {background: cyan; padding: 10px}
     `);
 let style = document.createElement('style');
 style.type = 'text/css';
 style.appendChild(stylesText);
 document.head.appendChild(style); // Инициализируем на странице
-
-//  Создаём Секцию для вывода елементов массива
-let section = document.createElement('section');
-section.id = 'section';
-let innerSection = document.createTextNode(array.forEach((val, index, array)=>{ return val }));
-section.appendChild(innerSection);
-document.body.appendChild(section);
 
 //  Создаём label для InputText
 let labelText = document.createElement('label');
@@ -40,7 +36,6 @@ labelElem.for = 'element';
 labelElem.innerText = 'Напишите индекс елемента';
 document.body.appendChild(labelElem);
 
-
 //  Создаём input для ввода номер елемента после или до которого вставлять
 let inputElem = document.createElement('input');
 inputElem.id = 'element';
@@ -58,5 +53,37 @@ let button_after = document.createElement('button');
 button_after.innerText = 'Вставить после';
 button_after.id = 'button_after';
 document.body.appendChild(button_after);
+
+//  Создаём Секцию для вывода елементов массива
+let section = document.createElement('section');
+section.id = 'section';
+document.body.appendChild(section);
+let innerSection = '';
+array.forEach(function (value, index) {
+    innerSection += `${value}[${index}] `
+});
+section.innerHTML = innerSection;
+
+let arrayText = document.getElementById('text');
+
+let arrayIndex = document.getElementById('element');
+
+document.getElementById('button_before').addEventListener('click', function () {
+    array.splice(Number(arrayIndex.value), 0, arrayText.value);
+    let arrayString = '';
+    array.forEach(function (value, index) {
+        arrayString += `${value}[${index}] `
+    });
+    section.innerHTML = arrayString;
+});
+
+document.getElementById('button_after').addEventListener('click', function () {
+    array.splice(Number(arrayIndex.value) + 1, 0, arrayText.value);
+    let arrayString = '';
+    array.forEach(function (value, index) {
+        arrayString += `${value}[${index}] `
+    });
+    section.innerHTML = arrayString;
+});
 
 
