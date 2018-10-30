804574733013b3e1057a9f41cdf46b2daaa9c78f
Maksim <mbkkong@gmail.com>
2018-10-30 14:53:54 +0200

    Сделал ДЗ 6 / Часть 2. Рекурсивное удаление

--- homework6/work_2.js
+++ homework6/work_2.js
@@ -22,20 +22,29 @@ elements.filter ( function ( element ) {
     return element.tagName === "DIV"
 })
     .forEach ( function ( element, num ) {
-        element.style = divStyle
+        element.style = divStyle;
         element.innerText = num
-    })
+    });
 
 elements.filter ( function ( element ) {
     return element.tagName === "BUTTON"
 })
     .forEach ( function ( element ) {
-        element.innerHTML = "remove DIVs"
+        element.innerHTML = "remove DIVs";
         element.onclick = function ( event ) {
             recursRemove ()
         }
-    })
+    });
 
-function recursRemove() {
+function recursRemove( ) {
+    let divs = document.getElementsByTagName('div');
+    if (divs.length > 0)
+    {
+        console.log(divs);
+        divs[0].remove();
+        return recursRemove()
+    }
+    else
+        console.log('udalil vsyo ep');
 
 }
